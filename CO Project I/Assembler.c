62f3a04b55c8f77c32f7cc985379df5d7bc21d50
MikeZed <39437367+MikeZed@users.noreply.github.com>
2019-12-06 19:06:55 +0200

    made some amazing changes

--- CO Project I/Assembler.c
+++ CO Project I/Assembler.c
@@ -1,6 +1,8 @@
 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TRUE 1 
 #define FALSE 0
@@ -12,55 +14,148 @@
 
 typedef	struct label {
 
-	char* name; 
+	char name[MAX_LABEL_LEN];
 	int address;
 
 } Label;
 
 
-int get_labels(); 
+void read_file();
 int check_label();
 int get_instruction(); 
 int get_opcode();
-
-
 
 int main(int argc, char** argv)
 {
 
 	FILE * ASM_file = NULL; 
-	Label* Labels[MAX_LINES];
+	FILE * MEMIN = NULL; 
 
+	Label* Labels[MAX_LINES];
 
 //	for (int i = 0; i < argc; i++)
 //	{
 //		printf("%s\n", argv[i]);
 //	}
 	
+	ASM_file = fopen(argv[2], "r");
+	MEMIN = fopen(argv[3], "w");
 
-	ASM_file = fopen(argv[2], 'r'); 
-
-	if (ASM_file == NULL)
+	if (ASM_file == NULL || MEMIN == NULL)
 	{
 		printf("Couldn't open file, terminating process\n");
 		exit(1); 
 	}
 
+	// read file, perform first pass 
+	read_file(ASM_file, Labels, 1, NULL); 
+
+	// read file, perform second pass 
+	read_file(ASM_file, Labels, 2, MEMIN);
 
 	fclose(ASM_file);
-
-
+	fclose(MEMIN);
 
 	return 0; 
-
 }
 
 
 // performs first pass over the code
 // to get the labels and their addresses
+void read_file(FILE* asm_file, Label* Labels[], int pass_num, FILE* out_file)
+{
 
-int get_labels();
+	char line[MAX_LINE_LEN];
+	char* delim = " \n\t,";    // line delimiters 
 
+	char* token;               // a token, will hold part of the line
+	int comment_start = FALSE; // if a token contains '#' it means a comment has started
+
+	char* number_sign_index = NULL;
+
+	int i = 1;
+	
+	while (fgets(line, MAX_LINE_LEN, asm_file) != NULL && i<100) // read file line by line 
+	{
+		token = strtok(line, delim); // get first token in line
+
+		while (token != NULL && *token != '#')
+		{
+
+			number_sign_index = strchr(token, '#'); // number_sign - '#', holds address of '#' in token if exists
+
+			if (number_sign_index != NULL)  // check if token has '#'
+			{
+				*number_sign_index = '\0';  // truncate token 
+				comment_start = TRUE;       // indicates that after current token a comment has started
+			}
+
+			// do stuff with token 
+
+
+
+
+
+
+
+
+			// first pass - get labels
+			if (pass_num == 1) 
+			{
+				check_label();
+
+			}
+			// second pass - get instructions in hex and write in file 
+			else
+			{
+
+				
+			};
+
+
+
+
+
+
+
+
+			// end 
+
+			
+			if (comment_start) // if a comment has started -> read next line 
+				break;
+
+			token = strtok(NULL, delim); // read next token 
+			
+		}
+	}
+}
+
+
+// checks if line contains a label
+// if it does return TRUE, otherwise FALSE 
+int check_label() 
+{
+
+
+
+}
+
+// recieves opcode str, returns opcode number in hex 
+int get_opcode()
+{
+
+
+
+}
+
+// recieves register str, returns register number in hex 
+int get_reg()
+{
+
+
+
+}
 
 
 int get_instruction();
