3d7385264e2099ff77bac5e7d020b36139c27b22
dvirbz <58605853+dvirbz@users.noreply.github.com>
2019-12-18 18:19:14 +0200

    Update Sort.asm

--- CO Project I/Sort.asm
+++ CO Project I/Sort.asm
@@ -24,7 +24,7 @@ Main:
 
 Sort: 		
 	   	limm $t0 , $0 , $0 , -6
-		add  $sp , $0 , $t0 , 0
+		add  $sp , $sp , $t0 , 0
 		sw   $s0 , $sp , $0 , 0 
 		sw   $s1 , $sp , $0 , 1
 		sw   $s2 , $sp , $0 , 2
@@ -45,6 +45,7 @@ Sort:
 			branch blt, $s0 , $s1 , Swap
 	IF:
 			branch bne, $vo , $0 , While
+			branch beq, $0 , $0 , End
 
 	Swap: 
 			sw  $s1 , $t2 , $0 , 0 #set A[j] = A[j+1]
@@ -59,6 +60,6 @@ Sort:
 			lw   $a1 , $sp , $0 , 4
 			lw   $v0 , $sp , $0 , 5
 			limm $t0 , $0 , $0 , 6
-			add  $sp , $0 , $t0, 0
+			add  $sp , $sp , $t0, 0
 			branch jr, $ra, $0, 0
 	
