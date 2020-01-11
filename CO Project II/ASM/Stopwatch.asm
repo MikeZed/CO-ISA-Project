82fd2d5bd1a7ddb1db68c0b401f1ecf7e9c449ef
MikeZed <39437367+MikeZed@users.noreply.github.com>
2020-01-11 20:28:38 +0200

    Update Stopwatch.asm

--- CO Project II/ASM/Stopwatch.asm
+++ CO Project II/ASM/Stopwatch.asm
@@ -1,1 +1,55 @@
 
+
+main: 
+							# intialize IO Registers 
+						
+		out $zero, $zero, $zero, 0		# intialize IORegister[0]
+		out $zero, $zero, $zero, 1		# intialize IO Registers[1]
+		out $zero, $zero, $zero, 2		# intialize IO Registers[2]
+		out $zero, $zero, $zero, 3		# intialize IO Registers[3]
+		out $zero, $zero, $zero, 4		# intialize IO Registers[4]
+		out $zero, $zero, $zero, 5		# intialize IO Registers[5]
+		
+		out $zero, $zero, $zero, 1		# enable interrupts 
+		
+			
+		jal $zero, $zero, $zero, cnt_time 	#  start counting 
+		
+	
+		
+		halt $zero, $zero, $zero, 0 		# halt
+
+
+
+
+
+cnt_time:						# updates time counters 
+						
+							# save total elapsed seconds
+							# save seconds
+							# save minutes
+						
+							# check if min is 1, 3 , 5...
+							# if true play music  
+
+
+		branch jr $ra, $zero, 0  		# return to main 
+
+
+
+
+hndl_int: 						# handles interrupts 
+
+							# handles interrupt 1 - Timer
+							# handles interrupt 2 - BTNC
+							# handles interrupt 3 - BTND 	
+
+
+		branch reti, $zero, $zero, 0 		# return to program
+
+
+
+
+
+
+
