71ea24926a03e7b00b412729cdec8fa61a21bb37
tamir0882 <58632401+tamir0882@users.noreply.github.com>
2020-01-15 18:53:57 +0200

    Update sim.h

--- CO Project II/Template.X/sim.h
+++ CO Project II/Template.X/sim.h
@@ -1,53 +1,53 @@
 #pragma once
 
+
 //#define _CRT_SECURE_NO_WARNINGS
 #define TRUE 1 
 #define FALSE 0
 
-#define MAX_LINES 4096 
+#define MAX_LINES 1000 
 #define SIZE 5
 #define LINE_SIZE 4
 #define REG_SIZE 16
-
+#define IOREG_SIZE 6
 
 int reg[REG_SIZE];
-char* memout[LINE_SIZE];
+char memout[MAX_LINES][SIZE];
 char* inst[LINE_SIZE];
-int pc , num_of_reg, address_of_mem;
-int counter;
-char* ioReg[];
-int flag_SWT;
+int buttons[5], swts[8];
+int pc, num_of_reg, address_of_mem, flag_SWT, ioReg[IOREG_SIZE], inst_count;
 
 /*Utility func*/
+
 int getHex(char* source);
 int hex2int(char ch);
 int getAddress(int address);
+void REG_Init();
+void IOREG_Init();
 /*Create func*/
-void createTrace(FILE* trace, int pc, char line[SIZE], int* reg[REG_SIZE], int* count);
-void createLastFiles(FILE* memout, char memory_out[MAX_LINES][SIZE], int max_line_counter, FILE* regout, int* reg[REG_SIZE], FILE* count, int counter);
+//void createTrace(FILE* trace, int pc, char line[SIZE], int reg[REG_SIZE], int* count);
 
 /*Line Func*/
-void add(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void sub(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void mul(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void andf(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void orf(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void sll(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void sra(int rd, int rs, int rt, int* reg[REG_SIZE]);
-void limm(int rd, char memory_in[MAX_LINES][LINE_SIZE], int* reg[REG_SIZE], int pc);
-void branch(int rd, int rs, int rt, char memory_in[MAX_LINES][LINE_SIZE], int* reg[REG_SIZE], int* pc);
-void jal(int* reg[REG_SIZE], int* pc, char memory_in[MAX_LINES][LINE_SIZE]);
-void lw(int rd, int rs, int* reg[REG_SIZE], char memory_out[MAX_LINES][SIZE], int pc);
-void sw(int rd, int rs, int* reg[REG_SIZE], char memory_in[MAX_LINES][LINE_SIZE], char memory_out[MAX_LINES][SIZE], int pc, int *max_line_counter);
-void halt(int* pc);
-void decipher_line(char line[SIZE], int* reg[REG_SIZE], char memory_in[MAX_LINES][LINE_SIZE], char memory_out[MAX_LINES][SIZE], int* pc, int *max_line_counter_ptr);
+void add(int rd, int rs, int rt);
+void sub(int rd, int rs, int rt);
+void mul(int rd, int rs, int rt);
+void andf(int rd, int rs, int rt);
+void orf(int rd, int rs, int rt);
+void sll(int rd, int rs, int rt);
+void sra(int rd, int rs, int rt);
+void limm(int rd, char memin[MAX_LINES][SIZE]);
+void branch(int rd, int rs, int rt, char memin[MAX_LINES][SIZE]);
+void jal(char memin[MAX_LINES][SIZE]);
+void lw(int rd, int rs);
+void sw(int rd, int rs, char memin[MAX_LINES][SIZE], int *max_line_counter);
+void in(int rd, int rs, char memin[MAX_LINES][SIZE]);
+void out(int rd, int rs, char memin[MAX_LINES][SIZE]);
+void halt();
+void decipher_line(char line[SIZE], char memin[MAX_LINES][SIZE], int *max_line_counter_ptr);
 
+int sim(char memin[MAX_LINES][SIZE]);
 
-
-
-int sim(int argc, const char* argv[]);
-
-char* fib[] = {"7D00",
+char fib[MAX_LINES][SIZE] = { "7D00",
 "0080",
 "A300",
 "0040",
@@ -111,5 +111,5 @@ char* fib[] = {"7D00",
 "0000",
 "0000",
 "0000",
-"0007"};
-char* stopper[] = {};
+"0007" };
+char stopper[MAX_LINES][SIZE] = { "" };
